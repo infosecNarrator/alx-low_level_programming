201bc91d7bcba60169ac19578ea7db955c007e73
infosec Narrator <infosecnarrator@gmail.com>
2022-04-12 20:04:00 -0700

    eleventh

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include  <stdio.h>
+
+/**
+ * prints numbers from n to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if ( n < 98)
+	{
+		for (; n <98; n++)
+		{
+			printf("%d, ",n);
+		}
+	}
+	printf("%d\n",n);
+}
